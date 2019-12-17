cf68f0364855e68f87b9e4764c112681bbf1a2dd
easbar <easbar.mail@posteo.net>
2019-12-17 14:51:12 +0100

    Replace supports(TurnWeighting.class) with supportsTurnCosts() in FlagEncoder

--- core/src/test/java/com/graphhopper/routing/RoutingAlgorithmTest.java
+++ core/src/test/java/com/graphhopper/routing/RoutingAlgorithmTest.java
@@ -26,7 +26,6 @@ import com.graphhopper.routing.querygraph.QueryGraph;
 import com.graphhopper.routing.util.*;
 import com.graphhopper.routing.weighting.FastestWeighting;
 import com.graphhopper.routing.weighting.ShortestWeighting;
-import com.graphhopper.routing.weighting.TurnWeighting;
 import com.graphhopper.routing.weighting.Weighting;
 import com.graphhopper.storage.*;
 import com.graphhopper.storage.index.LocationIndex;
@@ -59,8 +58,9 @@ import static org.junit.Assert.*;
 /**
  * This test tests the different routing algorithms on small user-defined sample graphs. It tests node- and edge-based
  * algorithms, but does *not* use turn costs, because otherwise node- and edge-based implementations would not be
- * comparable. For edge-based traversal u-turns cannot happen even when we are not using a {@link TurnWeighting}, because
- * as long as we do not apply turn restrictions we will never take a u-turn.. All tests should follow the same pattern:
+ * comparable. For edge-based traversal u-turns cannot happen even when the u-turn costs are zero, because because
+ * as long as we do not apply turn restrictions we will never take a u-turn.
+ * All tests here should follow the same pattern:
  * <p>
  * - create a GH storage, you need to pass all the weightings used in this test to {@link #createGHStorage}, such that
  * the according CHGraphs can be created
@@ -1002,7 +1002,7 @@ public class RoutingAlgorithmTest {
     private GraphHopperStorage createGHStorage(boolean is3D, Weighting... weightings) {
         CHProfile[] chProfiles = new CHProfile[weightings.length];
         for (int i = 0; i < weightings.length; i++) {
-            chProfiles[i] = new CHProfile(weightings[i], traversalMode, TurnWeighting.INFINITE_U_TURN_COSTS);
+            chProfiles[i] = new CHProfile(weightings[i], traversalMode, Weighting.INFINITE_U_TURN_COSTS);
         }
         return new GraphBuilder(encodingManager).set3D(is3D)
                 .setCHProfiles(chProfiles)
@@ -1155,7 +1155,7 @@ public class RoutingAlgorithmTest {
     private static abstract class CHCalculator implements PathCalculator {
         @Override
         public Path calcPath(GraphHopperStorage graph, Weighting weighting, TraversalMode traversalMode, int maxVisitedNodes, int from, int to) {
-            CHProfile chProfile = new CHProfile(weighting, traversalMode, TurnWeighting.INFINITE_U_TURN_COSTS);
+            CHProfile chProfile = new CHProfile(weighting, traversalMode, Weighting.INFINITE_U_TURN_COSTS);
             PrepareContractionHierarchies pch = PrepareContractionHierarchies.fromGraphHopperStorage(graph, chProfile);
             CHGraph chGraph = graph.getCHGraph(chProfile);
             if (chGraph.getEdges() == chGraph.getOriginalEdges()) {
@@ -1181,7 +1181,7 @@ public class RoutingAlgorithmTest {
 
         @Override
         public Path calcPath(GraphHopperStorage graph, Weighting weighting, TraversalMode traversalMode, int maxVisitedNodes, QueryResult from, QueryResult to) {
-            CHProfile chProfile = new CHProfile(weighting, traversalMode, TurnWeighting.INFINITE_U_TURN_COSTS);
+            CHProfile chProfile = new CHProfile(weighting, traversalMode, Weighting.INFINITE_U_TURN_COSTS);
             PrepareContractionHierarchies pch = PrepareContractionHierarchies.fromGraphHopperStorage(graph, chProfile);
             CHGraph chGraph = graph.getCHGraph(chProfile);
             if (chGraph.getEdges() == chGraph.getOriginalEdges()) {
